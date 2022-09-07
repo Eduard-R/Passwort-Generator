4b9379c1cc061f11a4f4a238e60f9253b672e12a
Eduard Richert <erichert@conet.de>
2022-09-07 20:28:43 +0200

    update

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const package = require('./package.json');
+const password = require('./createPassword');
+const createPassword = require('./createPassword');
+
+const port = process.env.port || process.env.PORT || 5000;
+const apiRoot = '/api';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ origin: /http:\/\/localhost/ }));
+app.options('*', cors());
+
+const router = express.Router();
+
+// Route Configurations
+router.get('/', (req, res) => {
+    res.send(`${package.description} - v${package.version}`);
+});
+
+router.get('/password', (req, res) => {
+    res.send(password());
+});
+
+app.use(apiRoot, router);
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port} ...`);
+}); 
